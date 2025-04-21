d9460056a917b9efcce91072f9f2c5226a314cc5
Sim0n-sk8 <simonswart91@gmail.com>
2025-04-21 20:50:51 +0200

    Still broken lol

--- script.js
+++ script.js
@@ -4,6 +4,59 @@ const pause = document.getElementById("pause");
 const stop = document.getElementById("stop");
 const input = document.getElementById("input");
 
-let interval = null;
-let timeLeft = 0;
-let isRunning = false;
+let time = 0;
+let running = false;
+let interval;
+
+function showTime() {
+    let minutes = Math.floor(time/60);
+    let seconds = time % 60;
+
+    if (minutes < 10) minutes = "0" + minutes;
+    if (seconds < 10) seconds = "0" + seconds;
+
+    timer.textContent = minutes + ":" + seconds;
+
+}
+
+input.addEventListener("change",()=> {
+    let mins = parseInt(input.value);
+    if(!isNaN(mins) && mins > 0){
+        time = mins * 60;
+        showTime();
+    }
+    
+});
+
+start.addEventListener("click" , () => {
+    if (running || time <= 0) return;
+
+    running = true;
+    interval = setInterval(() =>{
+        time--;
+        showTime;
+
+        if (time <= 0) {
+            clearInterval(interval);
+            running = false;
+
+            alert("Time is up!");
+
+        }
+    },1000);
+});
+
+
+pause.addEventListener("click", () => {
+    clearInterval(interval);
+    running = false;
+
+
+});
+
+stop.addEventListener("click", () =>{
+    clearInterval(interval);
+    running = false;
+    time = 0;
+    showTime();
+});
